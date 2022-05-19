2ff70fdcc6cd1c36e23737705560af8f5ff92a83
jj <swlgus03@gmail.com>
2022-05-20 08:43:35 +0900

    .

--- A.FrontDev/03_Javascript/ch06/test04.js
+++ A.FrontDev/03_Javascript/ch06/test04.js
@@ -1,19 +1,22 @@
-let brands = ["애플", "구글", "페이스북", "아마존", "삼성전자"];
+// continue문 
+// 1부터 10까지 수 중에 3으로 나눈 나머지가 1인 수만 더하기 
+// 1 4 7 10
+// 합 22 
 
-console.log(brands[0]);
-console.log(brands[1]);
-console.log(brands[2]);
-console.log(brands[3]);
-console.log(brands[4]);   //인덱스 
+let sum = 0; // 0 초기화
+for (let i=1; i<=10; i++) {
+    if (i%3 != 1){
+        continue; 
+    }
+     console.log(i);
+     sum += i; // sum = sum + i;
 
-// for문 
-// for(시작값; 최종값; 증감식) {여기 실행문이 계속 반복}
-
-for(let i=0; i<5; i++) {
-    console.log(brands[i]);
 }
+console.log("합은 " + sum);
 
 
-for(let i=0; i<brands.length; i++) {
-    console.log(brands[i]);
-}
+/**
+ * t:   2 3   5 6   8 9
+ * f: 1     4     7
+ * 
+ */
